cf8ed77521090eea54df5fb9b3190ac642656e0e
patrickeddy <p@patrickeddy.com>
2014-07-13 09:13:53 -0700

    More js examples

--- assets/js/engine.js
+++ assets/js/engine.js
@@ -29,10 +29,13 @@ var halfOfXVariable = halfOf(x);
 // Setting the returned value to the halfOfXVariable
 
 // You can also change elements in HTML by id with vanilla JS
-document.getElementById("myBody").innerHTML = "Hacked, muhaha!";
+function changeHTML() {
+    document.getElementById("myBody").innerHTML = "Hacked, muhaha!";
+}
 
 // To define a 'class, you make a 'function' act like an object in any other language...
 function dog(name, age, breed) {
+    this.name = name;
     this.age = age;
     this.breed = breed;
     this.personality = "";
@@ -40,7 +43,12 @@ function dog(name, age, breed) {
 
 // You then call the function as if you were creating a new object...
 var dog = new dog("Dilly", 14, "Poodle");
+dog.personality = "Yoked";
 
+function alertDogDetails() {
+    var details = "Name: " + dog.name + "\nBreed: " + dog.breed + "\nAge: " + dog.age;
+    alert(details);
+}
 /*
 
     Pretty much all you need to know atm.
